b19116a3fe633bc2d15ce51766987ed6ec978510
Anahit Grigoryan <grigoryan-anahit@mail.ru>
2021-11-29 13:12:09 +0400

    kkkkkkk

--- features/step-definitions/steps.js
+++ features/step-definitions/steps.js
@@ -48,34 +48,34 @@ Then (/^Should see search results$/,async ()=>{
     await expect(EpamPage.searchResult).toHaveTextContaining('AUTOMATION')
 });
           ///////////
-Given(/^I go to 'Our Work' page$/, async ()=>{
-    await browser.pause(1000)
-    await EpamPage.menuItemOurWork.click();
-});
-Then (/^Page title should be 'Explore our Work | EPAM Customer Stories, Brochures & Accelerators'$/,async ()=>{
-    await expect(browser).toHaveTitle('Explore our Work | EPAM Customer Stories, Brochures & Accelerators')
-});
-When (/^I fill 'Filter By' fields$/, async ()=>{
-     await  EpamPage.filterByDiv.scrollIntoView();
-    await browser.pause(1000)
-    await EpamPage.filterIndustries.click();
-    await browser.pause(2000)
-    await EpamPage.ffContent(2)
-     await EpamPage.filterIndustries.click();
-    await browser.pause(2000)
-
-     await  EpamPage.filterContentTypes.click();
-     await browser.pause(3000)
-    await EpamPage.ff(2);
-    await  EpamPage.filterContentTypes.click();
-    await browser.pause(2000);
-
-});
-Then(/^Should see filters$/,async ()=>{
-    await  expect(EpamPage.filterDisplayFirst).toHaveTextContaining("BUSINESS INFORMATION SERVICES");
-    await  expect(EpamPage.filterDisplaySecond).toHaveTextContaining("BROCHURES");
-    await browser.pause(1000)
-})
+// Given(/^I go to 'Our Work' page$/, async ()=>{
+//     await browser.pause(1000)
+//     await EpamPage.menuItemOurWork.click();
+// });
+// Then (/^Page title should be 'Explore our Work | EPAM Customer Stories, Brochures & Accelerators'$/,async ()=>{
+//     await expect(browser).toHaveTitle('Explore our Work | EPAM Customer Stories, Brochures & Accelerators')
+// });
+// When (/^I fill 'Filter By' fields$/, async ()=>{
+//      await  EpamPage.filterByDiv.scrollIntoView();
+//     await browser.pause(1000)
+//     await EpamPage.filterIndustries.click();
+//     await browser.pause(2000)
+//     await EpamPage.ffContent(2)
+//      await EpamPage.filterIndustries.click();
+//     await browser.pause(2000)
+//
+//      await  EpamPage.filterContentTypes.click();
+//      await browser.pause(3000)
+//     await EpamPage.ff(2);
+//     await  EpamPage.filterContentTypes.click();
+//     await browser.pause(2000);
+//
+// });
+// Then(/^Should see filters$/,async ()=>{
+//     await  expect(EpamPage.filterDisplayFirst).toHaveTextContaining("BUSINESS INFORMATION SERVICES");
+//     await  expect(EpamPage.filterDisplaySecond).toHaveTextContaining("BROCHURES");
+//     await browser.pause(1000)
+// })
 
 
 
